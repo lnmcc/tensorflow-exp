d4edee394167290dc786c5dea650836864bc2dd4
lnmcc <lnmcc@hotmail.com>
2018-07-21 00:44:17 +0800

    ..

--- readsplitfile.py
+++ readsplitfile.py
@@ -1,2 +1,27 @@
 import tensorflow as tf
 
+files = tf.train.match_filenames_once("/tmp/data.tfrecords-*")
+filename_queue = tf.train.string_input_producer(files, shuffle=False)
+
+reader = tf.TFRecordReader()
+_, serialized_example = reader.read(filename_queue)
+features = tf.parse_single_example(serialized_example,
+                features={
+                    'i': tf.FixedLenFeature([], tf.int64),
+                    'j': tf.FixedLenFeature([], tf.int64),
+                })
+with tf.Session() as sess:
+    tf.global_variables_initializer().run()
+   
+    print([str(i.name) for i in tf.local_variables()])
+
+    print(sess.run(files))
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    for i in range(6):
+        print(sess.run([features['i'], features['j']]))
+
+    coord.request_stop()
+    coord.join(threads)
