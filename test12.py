e410e01b08a7f2b6fcba8177e09fa0ee07bd0f55
lnmcc <lnmcc@hotmail.com>
2018-07-30 09:28:36 +0800

    ...

--- test12.py
+++ test12.py
@@ -3,7 +3,15 @@ import tensorflow as tf
 a = tf.constant(5)
 sess = tf.Session()
 
+ones = tf.ones([6])
+uniform = tf.random_uniform([3, 3, 3], minval=0, maxval=10)
+normal = tf.random_normal([3, 3, 3], mean=0.0, stddev=2.0)
+
 with sess.as_default():
     print(a.eval())
+    print(ones.eval())
+    print(uniform.eval())
+    print(normal.eval())
 
 sess.close()
+
